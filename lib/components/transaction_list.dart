97cd13d59021227432d217fd9f36841396a6f422
Pedro Barbosa <barbosaph1805@gmail.com>
2025-02-23 02:33:06 -0300

    Trocando o SingleChildScrollView de transaction_list para ListView

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,47 +9,52 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tr) {
-        return Card(
-            child: Row(children: <Widget>[
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-            decoration: BoxDecoration(
-                border: Border.all(
-              color: Colors.purple,
-              width: 2,
-            )),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              'R\$ ${tr.value.toStringAsFixed(2)}',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index){
+          final tr = transactions[index];
+          return Card(
+              child: Row(children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(
                 color: Colors.purple,
-              ),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                tr.title,
+                width: 2,
+              )),
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'R\$ ${tr.value.toStringAsFixed(2)}',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 16,
+                  fontSize: 20,
+                  color: Colors.purple,
                 ),
               ),
-              Text(
-                DateFormat('d MMM y').format(tr.date),
-                style: TextStyle(
-                  color: Colors.grey[700],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  tr.title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
                 ),
-              )
-            ],
-          ),
-        ]));
-      }).toList(),
+                Text(
+                  DateFormat('d MMM y').format(tr.date),
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                  ),
+                )
+              ],
+            ),
+          ]));
+        },
+      ),
     );
   }
 }
